3590c631600cc8b7c3a7df55305f659b0a6e1f49
admin-minecraft-server <leersarthur@gmail.com>
2025-07-28 22:25:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const linkInput = document.getElementById('link');
 const itemsContainer = document.getElementById('items');
 const searchInput = document.getElementById('search');
 
-const PASSWORD = 'admin_editor';
+const PASSWORD = 'admin';
 
 passwordInput.addEventListener('input', () => {
   if (passwordInput.value === PASSWORD) {
